e85c1cc41b9733dcae606712630e87662140ccae
tingLIU66 <tliu6@luc.edu>
2017-02-15 17:01:51 -0600

    Added DAO classes

---
+++ FacilityManagementSystem/src/dal/FacilityDAO.java
@@ -0,0 +1,39 @@
+package dal;
+
+import java.util.Set;
+
+import model.facility.Apartment;
+import model.facility.FacilityDetail;
+
+
+//o public object addNewFacility()
+//o public void addFacilityDetail()
+//o public object removeFacility() 
+
+public class FacilityDAO {
+	
+public Set<Apartment> listFacilities() {
+	return null;	
+		
+	}
+
+public FacilityDetail getFacilityInformation(int facilityID) {
+	
+	return null;
+}
+
+public int requestAvailableCapacity(int facilityID) {
+        
+	return 0;
+   
+  }
+
+public Apartment addNewFacility(){
+	return null;
+}
+
+
+public Set<Apartment> removeNewFacility(){
+	return null;
+}
+}
