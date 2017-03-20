ffd63533edab5da17def116e998cb7a5d9756d74
tingLIU66 <tliu6@luc.edu>
2017-03-19 22:22:39 -0500

    Modified facilityClient to adapt spring frame

--- FacilityManagementSystem/src/model/facility/FacilityAdmin.java
+++ FacilityManagementSystem/src/model/facility/FacilityAdmin.java
@@ -14,52 +14,46 @@ public class FacilityAdmin {
 	
 	FacilityDAO facilitydao = new FacilityDAO();
 	
-	public void setAdminName(String adminName){
-		this.adminName = adminName;
-	}
-    
-	public String getAdminName(){
+	public String getAdminName() {
 		return adminName;
 	}
-	
-	public void setAdminLocation(String adminLocation){
-		this.adminLocation = adminLocation;
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
 	}
-    
-	public String getAdminLocation(){
+
+	public String getAdminLocation() {
 		return adminLocation;
 	}
-	
-	public void setOfficePhoneNon(String officePhoneNo){
-		this.officePhoneNo = officePhoneNo;
+
+	public void setAdminLocation(String adminLocation) {
+		this.adminLocation = adminLocation;
 	}
-    
-	public String getOfficePhoneNo(){
+
+	public String getOfficePhoneNo() {
 		return officePhoneNo;
 	}
-	
-	public void setApartment(Set<Apartment> apartments){
-		
-		this.apartments = apartments;
+
+	public void setOfficePhoneNo(String officePhoneNo) {
+		this.officePhoneNo = officePhoneNo;
 	}
-	
-	public Set<Apartment> getApartment() {
-		
+
+	public Set<Apartment> getApartments() {
 		return apartments;
 	}
-	
-	public void setStaff(Set<Staff> staffs){
-		
-		this.staffs = staffs;
+
+	public void setApartments(Set<Apartment> apartments) {
+		this.apartments = apartments;
 	}
-	
-	public Set<Staff> getStaff() {
-		
+
+	public Set<Staff> getStaffs() {
 		return staffs;
 	}
-	
 
-	
+	public void setStaffs(Set<Staff> staffs) {
+		this.staffs = staffs;
+	}
+
 	public Set<Apartment> listFacilities() {
 		
 		return facilitydao.listFacilities();
--- FacilityManagementSystem/src/view/FacilityClient.java
+++ FacilityManagementSystem/src/view/FacilityClient.java
@@ -1,9 +1,12 @@
 package view;
 
-import java.util.HashSet;
+
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import model.facility.Apartment;
 import model.facility.FacilityAdmin;
@@ -14,30 +17,32 @@ public class FacilityClient {
 	public static void main(String args[]) throws Exception {
 
 	//Client will use the facility client to access to anything related to facility functionality.
+	
+	ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/facility-app.xml");
 	Scanner sc = new Scanner(System.in);
     System.out.println("*************** Creating Facility related objects *************************");
-    FacilityAdmin fadmin = new FacilityAdmin(); 
-    Apartment apt = new Apartment();
-    FacilityDetail fdetail = new FacilityDetail();
-    Set<Apartment> apartments = new HashSet<Apartment>();
-    apartments = fadmin.listFacilities();
+   
+    FacilityAdmin fadmin = (FacilityAdmin)context.getBean("facilityadmin");
+    Apartment apt = (Apartment)context.getBean("apartment");
+    FacilityDetail fdetail = (FacilityDetail)context.getBean("facilitydetail");
+    Set<Apartment> apartments = fadmin.listFacilities();
 	
-	System.out.println("*************List All the Facility under management ****************");
-	       
-	Iterator<Apartment> it = apartments.iterator();
-	while(it.hasNext()) {
-		Apartment apt1 = (Apartment) it.next();
-		
-		System.out.println("ApartmentID: " + apt1.getApartmentID());
-		System.out.println("ApartmentName: " + apt1.getName());
-		System.out.println();
-	}
+	System.out.println("-----------------List All the Facility under management: --------------------------------");
+		System.out.println();	       
+		Iterator<Apartment> it = apartments.iterator();
+		while(it.hasNext()) {
+			Apartment apt1 = (Apartment) it.next();		
+			System.out.println("ApartmentID: " + apt1.getApartmentID());
+			System.out.println("ApartmentName: " + apt1.getName());
+			System.out.println();
+		}
 	
-	System.out.println("*************Input ApartmentID to get Facility Information:*********************************");
+	System.out.println("-----------------Input ApartmentID to get Facility Information:------------------------------");
+	    System.out.println("Input apartmentID:");
 	   // Scanner sc = new Scanner(System.in);
 	    String aptID = sc.next();
 	    fdetail = apt.getFacilityInformation(aptID);
-	    System.out.println("============================================================================");   
+	    System.out.println();   
 		System.out.println("ApartmentID: " + aptID);
 		System.out.println("Address: " + fdetail.getAddress() );
 		System.out.println("Zipcode: " + fdetail.getZipcode());
@@ -46,46 +51,43 @@ public class FacilityClient {
 		System.out.println("Parking: " + fdetail.getParking());
 		System.out.println();
 	
-	System.out.println("*************** Add a new Facility to manage *****************************");
-	//Scanner sc1 = new Scanner(System.in);
-    System.out.println("Input apartmentID:");
-	String addaptid = sc.next();
-	System.out.println("Input apartment name:");
-    String name = sc.next();	
-	fadmin.adddNewFacility(addaptid, name);
-	System.out.println("Aartment( " + addaptid + ", " + name + ")added successfully!");
-	System.out.println();
+	System.out.println("---------------- Add a new Facility to manage: -----------------------------------------------");
+		//Scanner sc1 = new Scanner(System.in);
+	    System.out.println("Input apartmentID:");
+		String addaptid = sc.next();
+		System.out.println("Input apartment name:");
+	    String name = sc.next();	
+		fadmin.adddNewFacility(addaptid, name);
+		System.out.println("Aartment( " + addaptid + ", " + name + ")added successfully!");
+		System.out.println();
 	
+	System.out.println("-----------------Remove a new Facility to manage:-----------------------------------------------");
+		System.out.println("Input apartmentID you want you remove:"); 
+		String delaptid = sc.next();
+		System.out.println(fadmin.removeFacility(delaptid));	
+		System.out.println();
+		
+	System.out.println("-----------------Request the number of units available in a specific apartment:------------------");
+		System.out.println("Input apartmentID:");
+		String reqaptID = sc.next();
+		int unitavb = apt.requestAvailableCapacity(reqaptID);
+		System.out.println("Apartment " + reqaptID + " has " + unitavb + " units available");
+		System.out.println();
 	
-	
-	System.out.println("*************** Remove a new Facility to manage *****************************");
-	System.out.println("Input apartmentID you want you remove:"); 
-	String delaptid = sc.next();
-	System.out.println(fadmin.removeFacility(delaptid));	
-	System.out.println();
-	
-	System.out.println("***************Request the number of units available in a specific apartment*****************************");
-	System.out.println("Input apartmentID:");
-	String raptID = sc.next();
-	int unitavb = apt.requestAvailableCapacity(raptID);
-	System.out.println("Apartment " + raptID + " has " + unitavb + " units available");
-	System.out.println();
-	
-	System.out.println("***************Add detail to a new added apartment*****************************");
-	System.out.println("Input apartmentID:");
-	String adaptID = sc.next();
-	//Initiate a facilitydetail
-	FacilityDetail fdet = new FacilityDetail();
-	fdet.setAdress("601 W Jackson Blvd, Chicago, IL");
-	fdet.setAge(10);
-	fdet.setCapacity(135);
-	fdet.setParking("Yes");
-	fdet.setZipcode("60661");
-	
-    System.out.println("============================================================================");   
-	System.out.println(apt.addFacilityDetail(adaptID, fdet));
-	
-	System.out.println();
+	System.out.println("-----------------Add detail to a new added apartment:---------------------------------------------");
+		System.out.println("Input apartmentID:");
+		String addtaptID = sc.next();
+		//Initiate a facilitydetail
+		FacilityDetail fdetail2 = (FacilityDetail)context.getBean("facilitydetail");
+		fdetail2.setAdress("601 W Jackson Blvd, Chicago, IL");
+		fdetail2.setAge(10);
+		fdetail2.setCapacity(135);
+		fdetail2.setParking("Yes");
+		fdetail2.setZipcode("60661");
+		
+	    System.out.println();   
+		System.out.println(apt.addFacilityDetail(addtaptID, fdetail2));		
+		System.out.println();
 }		
 	
 }
